d16c612d6f7e6c1f9cb24ea02894366e55e9ad5a
root <root@DESKTOP-L3SM155.localdomain>
2023-06-24 17:45:44 +0530

    test lambda invoke

---
+++ lambda-2-lambda-vpc-endpoint/lambda.py
@@ -0,0 +1,27 @@
+# Creat VPC Endpoint (Interface) for Lambda
+# Select relevant Subnets and SGs for the Endpoint
+# Just ensure the Endpoint is reachable from relevant Lambdas inside the Subnet
+# During Test All Traffic was allowed for Subnet CIDR in the SG used
+# Private DNS Resolver (Outbound) has caused issues in tests
+# The VPC Endpoint will publish a few DNS names which are to be used in the code when invoking the other lambda
+# The following code invoke lambda l2 using the VPC endpoint DNS in endpoint_url
+# boto3 is part of default lambda so extra module need not be loaded
+
+import json
+import boto3
+
+c = boto3.client('lambda', endpoint_url='https://vpce-0b7ddd1a692a66753-wuc3oi7r.lambda.us-east-1.vpce.amazonaws.com')
+
+def lambda_handler(event, context):
+    # TODO implement
+    print("Triggered !!!")
+
+    r = c.invoke(
+        FunctionName = "arn:aws:lambda:us-east-1:456235575972:function:l2"
+        )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
